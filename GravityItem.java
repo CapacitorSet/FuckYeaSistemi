e00c3c497235da9cace3475ee89df1c36d4e560a
CapacitorSet <nobody@localhost>
2015-03-03 20:33:16 +0100

    Reordered some stuff, Preys now have a maximum speed

--- GravityItem.java
+++ GravityItem.java
@@ -1,7 +1,7 @@
 
 public abstract class GravityItem extends Item {
 
-    public float WaterSourceFactor, PreyFactor;
+    public float maxSpeed;
     public float xVelocity, yVelocity, zVelocity;
     public static final int G = 1;
 
@@ -10,39 +10,15 @@ public abstract class GravityItem extends Item {
                 + Math.pow(a.y - this.y, 2));
     }
 
-    public Vector accelerationTo(Item a) {
+    public Vector accelerationTo(Item a, float weight) {
         Vector result = new Vector();
-        double thing = G / Math.pow(distanceTo(a), 3);
-        if (a instanceof WaterSource)
-            thing *= WaterSourceFactor;
-        else if (a instanceof Prey)
-            thing *= PreyFactor;
-        else
-            thing = 0;
+        double thing = G / Math.pow(distanceTo(a), 3) * weight;
         result.dX = (a.x - this.x) * thing;
         result.dY = (a.y - this.y) * thing;
         return result;
     }
-
-    public Vector totalAcceleration(Item[] system) {
-        Vector result = new Vector();
-        for (Item a : system) {
-            if (a != this) {
-                result.add(accelerationTo(a));
-            }
-        }
-        return result;
-    }
-
-    public void updateVelocity(Item[] system, float dt) {
-        Vector acceleration = totalAcceleration(system);
-        System.out.println(acceleration.dX);
-        this.xVelocity += dt * acceleration.dX;
-        this.yVelocity += dt * acceleration.dY;
-    }
-
-    public void updatePosition(float dt) {
-        this.x += dt * this.xVelocity;
-        this.y += dt * this.yVelocity;
+    
+    public Vector accelerationTo(Item a) {
+        return accelerationTo(a, 1);
     }
 }
--- Prey.java
+++ Prey.java
@@ -2,22 +2,53 @@
 import java.awt.Color;
 
 public class Prey extends GravityItem {
+    
+    public int WaterSourceFactor;
 
     Prey(int x, int y) {
         this.color = Color.GREEN;
         this.x = x;
         this.y = y;
         this.WaterSourceFactor = 10000;
+        this.maxSpeed = 50;
+    }
+    
+    public Vector totalAcceleration(Item[] system) {
+        Vector result = new Vector();
+        for (Item a : system) {
+            if (a != this) {
+                if (a instanceof WaterSource) {
+                    if (distanceTo(a)<10) {
+                        this.WaterSourceFactor = 0; // If in the vicinity of a WaterSource, drink and don't get any closer
+                    } else {
+                        result.add(accelerationTo(a, WaterSourceFactor));
+                    }
+                } else {
+                    // Do nothing, object not identified
+                }
+            }
+        }
+        return result;
     }
     
     @Override
     public void velocityTick(Item[] sistema, float dt) {
-        updateVelocity(sistema, dt);
+        Vector acceleration = totalAcceleration(sistema);
+        this.xVelocity += dt * acceleration.dX;
+        this.yVelocity += dt * acceleration.dY;
+        float speed = (float) Math.sqrt(Math.pow(this.xVelocity, 2) + Math.pow(this.yVelocity, 2));
+        if (speed > this.maxSpeed) {
+            this.xVelocity = (this.xVelocity / speed) * this.maxSpeed;
+            this.yVelocity = (this.yVelocity / speed) * this.maxSpeed;
+        }
+        // Todo: limit max self velocity
+        this.WaterSourceFactor += 1000;
     }
 
     @Override
     public void positionTick(float dt) {
-        updatePosition(dt);
+        this.x += dt * this.xVelocity;
+        this.y += dt * this.yVelocity;
     }
     
 }
