fb7457356f75717358e371813a2c409ffa6382b3
CapacitorSet <nobody@localhost>
2015-03-05 23:40:29 +0100

    Bugfix

--- Item.java
+++ Item.java
@@ -1,8 +1,9 @@
 
 import java.awt.Color;
+import java.util.List;
 
 abstract public class Item extends Point {
-    abstract public void velocityTick(Item[] sistema, float dt);
+    abstract public void velocityTick(List<Item> sistema, float dt);
     abstract public void positionTick(float dt);
     public Color color;
 }
