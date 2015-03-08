36e3c2aab549802de2089faed2e38ffb6b1e2076
CapacitorSet <nobody@localhost>
2015-03-08 16:19:37 +0100

    Removed birth code, to be implemented at a later point

--- src/Predator.java
+++ src/Predator.java
@@ -25,19 +25,6 @@ public class Predator extends Individual {
 		 * not negative (actively escaping from water sources).
 		 */
 		factor.put("WaterSource", (thirst > 0 ? thirst : 0));
-		//birthTick();
-	}
-
-	public void birthTick() {
-		if (random.nextInt(100) == 0) {
-			simulazione.scheduleBirth(
-					new Prey(simulazione, this.x + random.nextInt(20), this.y + random.nextInt(20))
-			);
-		}
-		/* On each tick, there's a 1/100 chance of a new Prey being spawned
-		 * (actually, being scheduled for birth) within 20 pixels of the
-		 * present one
-		 */
 	}
 
 	public void interactWith(Item i) {
--- src/Simulazione.java
+++ src/Simulazione.java
@@ -6,7 +6,6 @@ import static org.lwjgl.opengl.GL11.*;
 public class Simulazione extends JFrame {
 
     ArrayList<Item> sistema; // The system at the present state
-    ArrayList<Item> births, deaths; // Scheduled births and deaths
     float dt;       // The simulation step (previous state was "t", next is "t+dt")
 
     static int delay = 50; // The real-life delay between one step and another
@@ -17,15 +16,6 @@ public class Simulazione extends JFrame {
         sistema.add(new WaterSource(this, 200, 50));
         sistema.add(new Prey(this, 80, 80));
 		sistema.add(new Predator(this, 180, 180));
-        births = new ArrayList<Item>();
-    }
-
-    public void scheduleBirth(Item i) {
-        this.births.add(i);
-    }
-    
-    void spawn(Item i) {
-        sistema.add(i);
     }
     
     public void tick() { // Redraw the items
@@ -47,10 +37,5 @@ public class Simulazione extends JFrame {
             a.positionTick(dt);          // Do a "position tick" on each item.
             // Note: should probably be inlined, since the behaviour can't be customized
         }
-        
-        for (Item a : births) {
-            this.spawn(a);
-        }
-        births.clear();
     }
 }
